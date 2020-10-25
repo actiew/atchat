389ef2df8d8c9bbe5ecb5d262eab058b4982132a
Actiew <wuwuzgr@gmail.com>
2020-10-25 23:47:29 +0800

    Storybook配置
    Signed-off-by: Actiew <wuwuzgr@gmail.com>

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import {addDecorator} from "@storybook/react";
+import {ThemeProvider} from "styled-components";
+import theme from "theme";
+
+export const parameters = {
+   actions: {argTypesRegex: "^on[A-Z].*"},
+}
+
+addDecorator(storyFn => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>)
