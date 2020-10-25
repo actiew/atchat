b883468dab745d6964691a308deb9fd00f464718
Actiew <wuwuzgr@gmail.com>
2020-10-25 23:48:01 +0800

    主题
    Signed-off-by: Actiew <wuwuzgr@gmail.com>

---
+++ src/theme.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+   primaryColor: "#4F6F9F",
+   greenColor: "#66CC55",
+   redColor: "#EE6655",
+   whiteColor: "#FFF",
+}
