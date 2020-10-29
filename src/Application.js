d32bd76ed584b02cbe30d43371e1e5492cd5f958
Actiew <wuwuzgr@gmail.com>
2020-10-29 21:57:40 +0800

    atchat
    Signed-off-by: Actiew <wuwuzgr@gmail.com>

--- src/Application.js
+++ src/Application.js
@@ -1,11 +1,18 @@
+import "Application.css";
+import { ChatApp } from "components/ChatApp/ChatApp";
 import React from "react";
-import 'Application.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+import theme from "theme";
 
 function Application() {
    return (
-      <div className="Application">
-      
-      </div>
+      <Router>
+         <ThemeProvider theme={theme}>
+            <ChatApp/>
+         </ThemeProvider>
+      </Router>
    );
 }
 
