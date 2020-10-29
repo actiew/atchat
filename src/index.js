bae5fa87f35de582649adb215e69d3cd13dfe701
Actiew <wuwuzgr@gmail.com>
2020-10-29 21:56:27 +0800

    atchat
    Signed-off-by: Actiew <wuwuzgr@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Application from "Application";
+import "index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+   <React.StrictMode>
+      <Application/>
+   </React.StrictMode>
+   , document.getElementById("actiew"),
+);
