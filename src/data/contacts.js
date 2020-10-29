e34c70bb1cef0e83d43477fa77285d32bde1398a
Actiew <wuwuzgr@gmail.com>
2020-10-29 21:31:15 +0800

    好友列表数据
    Signed-off-by: Actiew <wuwuzgr@gmail.com>

---
+++ src/data/contacts.js
@@ -0,0 +1,164 @@
+import avatar001 from "resources/images/avatar001.png";
+import avatar002 from "resources/images/avatar002.png";
+import avatar003 from "resources/images/avatar003.png";
+import avatar004 from "resources/images/avatar004.png";
+import avatar005 from "resources/images/avatar005.png";
+import avatar006 from "resources/images/avatar006.png";
+import avatar007 from "resources/images/avatar007.png";
+import avatar008 from "resources/images/avatar008.png";
+import avatar009 from "resources/images/avatar009.png";
+import avatar010 from "resources/images/avatar010.png";
+import avatar011 from "resources/images/avatar011.png";
+import avatar012 from "resources/images/avatar012.png";
+import avatar013 from "resources/images/avatar013.png";
+import avatar014 from "resources/images/avatar014.png";
+import avatar015 from "resources/images/avatar015.png";
+import avatar016 from "resources/images/avatar016.png";
+import avatar017 from "resources/images/avatar017.png";
+import avatar018 from "resources/images/avatar018.png";
+import avatar019 from "resources/images/avatar019.png";
+import avatar020 from "resources/images/avatar020.png";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 1,
+    avatar: avatar001,
+    name: "李铭浩",
+    intro: "这是一段个性签名，这个人很懒",
+    status: "online",
+  },
+  {
+    id: 2,
+    avatar: avatar002,
+    name: "孙文佳",
+    intro: "我是一名前端工程师",
+    status: "online",
+  },
+  {
+    id: 3,
+    avatar: avatar003,
+    name: "慕容天宇",
+    intro: "千里之行，始于足下。",
+    status: "offline",
+  },
+  {
+    id: 4,
+    avatar: avatar004,
+    name: "郭文菲",
+    intro: "一寸光阴一寸金，寸金难买寸光阴",
+    status: "online",
+  },
+  {
+    id: 5,
+    avatar: avatar005,
+    name: "张峰",
+    intro: "玉不琢、不成器，人不学、不知义",
+    status: "online",
+  },
+  {
+    id: 6,
+    avatar: avatar006,
+    name: "李凝",
+    intro: "读书破万卷，下笔如有神",
+    status: "offline",
+  },
+  {
+    id: 7,
+    avatar: avatar007,
+    name: "王雨斐",
+    intro: "莫等闲，白了少年头，空悲切",
+    status: "online",
+  },
+  {
+    id: 8,
+    avatar: avatar008,
+    name: "赵子枫",
+    intro: "学而不思则罔，思而不学则殆",
+    status: "offline",
+  },
+  {
+    id: 9,
+    avatar: avatar009,
+    name: "李铭浩",
+    intro: "这是一段个性签名，这个人很懒",
+    status: "online",
+  },
+  {
+    id: 10,
+    avatar: avatar010,
+    name: "孙文佳",
+    intro: "我是一名前端工程师",
+    status: "online",
+  },
+  {
+    id: 11,
+    avatar: avatar011,
+    name: "慕容天宇",
+    intro: "千里之行，始于足下。",
+    status: "offline",
+  },
+  {
+    id: 12,
+    avatar: avatar012,
+    name: "郭文菲",
+    intro: "一寸光阴一寸金，寸金难买寸光阴",
+    status: "online",
+  },
+  {
+    id: 13,
+    avatar: avatar013,
+    name: "张峰",
+    intro: "玉不琢、不成器，人不学、不知义",
+    status: "online",
+  },
+  {
+    id: 14,
+    avatar: avatar014,
+    name: "李凝",
+    intro: "读书破万卷，下笔如有神",
+    status: "offline",
+  },
+  {
+    id: 15,
+    avatar: avatar015,
+    name: "王雨斐",
+    intro: "莫等闲，白了少年头，空悲切",
+    status: "online",
+  },
+  {
+    id: 16,
+    avatar: avatar016,
+    name: "赵子枫",
+    intro: "学而不思则罔，思而不学则殆",
+    status: "offline",
+  },
+  {
+    id: 17,
+    avatar: avatar017,
+    name: "张峰",
+    intro: "玉不琢、不成器，人不学、不知义",
+    status: "online",
+  },
+  {
+    id: 18,
+    avatar: avatar018,
+    name: "李凝",
+    intro: "读书破万卷，下笔如有神",
+    status: "offline",
+  },
+  {
+    id: 19,
+    avatar: avatar019,
+    name: "王雨斐",
+    intro: "莫等闲，白了少年头，空悲切",
+    status: "online",
+  },
+  {
+    id: 20,
+    avatar: avatar020,
+    name: "赵子枫",
+    intro: "学而不思则罔，思而不学则殆",
+    status: "offline",
+  },
+];
