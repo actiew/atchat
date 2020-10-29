92f4d2fb649d14b64aa2b10fc3f7c9e9023c266c
Actiew <wuwuzgr@gmail.com>
2020-10-29 21:30:54 +0800

    消息列表数据
    Signed-off-by: Actiew <wuwuzgr@gmail.com>

---
+++ src/data/messages.js
@@ -0,0 +1,224 @@
+import avatar001 from "resources/images/avatar001.png";
+import avatar002 from "resources/images/avatar002.png";
+import avatar003 from "resources/images/avatar003.png";
+import avatar004 from "resources/images/avatar004.png";
+import avatar005 from "resources/images/avatar005.png";
+import avatar006 from "resources/images/avatar006.png";
+import avatar007 from "resources/images/avatar007.png";
+import avatar008 from "resources/images/avatar008.png";
+import avatar009 from "resources/images/avatar009.png";
+import avatar010 from "resources/images/avatar010.png";
+import avatar011 from "resources/images/avatar011.png";
+import avatar012 from "resources/images/avatar012.png";
+import avatar013 from "resources/images/avatar013.png";
+import avatar014 from "resources/images/avatar014.png";
+import avatar015 from "resources/images/avatar015.png";
+import avatar016 from "resources/images/avatar016.png";
+import avatar017 from "resources/images/avatar017.png";
+import avatar018 from "resources/images/avatar018.png";
+import avatar019 from "resources/images/avatar019.png";
+import avatar020 from "resources/images/avatar020.png";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 1,
+    avatarSrc: avatar001,
+    name: "李铭浩",
+    status: "online",
+    time: "3 小时之前",
+    message: "即使爬到最高的山上，一次也只能脚踏实地地迈一步",
+    unreadCount: 2,
+    replied: false,
+  },
+  {
+    id: 2,
+    avatarSrc: avatar002,
+    name: "实视",
+    status: "online",
+    time: "下午 14:45",
+    message: "对付敌人唯一的手段，打败ta！",
+    unreadCount: 2,
+    replied: false,
+  },
+  {
+    id: 3,
+    avatarSrc: avatar003,
+    name: "慕容天宇",
+    status: "offline",
+    time: "早上 06:18",
+    message: "明天约一把王者荣耀，不连赢5把不罢休 🤘",
+    unreadCount: 0,
+    replied: true,
+  },
+  {
+    id: 4,
+    avatarSrc: avatar004,
+    name: "郭文菲",
+    status: "online",
+    time: "2018年9月5日",
+    message: "明天我要去上课，如果有空的话我再和你说",
+    unreadCount: 0,
+    replied: true,
+  },
+  {
+    id: 5,
+    avatarSrc: avatar005,
+    name: "张峰",
+    status: "online",
+    time: "2018年9月1日",
+    message: "公司打印机坏了，找个人维修一下",
+    unreadCount: 0,
+    replied: false,
+  },
+  {
+    id: 6,
+    avatarSrc: avatar006,
+    name: "李凝",
+    status: "offline",
+    time: "2018年8月31日",
+    message: "你知道吗？昨天发生了一件很神奇的事情！",
+    unreadCount: 0,
+    replied: true,
+  },
+  {
+    id: 7,
+    avatarSrc: avatar007,
+    name: "李铭浩",
+    status: "online",
+    time: "3 小时之前",
+    message: "即使爬到最高的山上，一次也只能脚踏实地地迈一步",
+    unreadCount: 2,
+    replied: false,
+  },
+  {
+    id: 8,
+    avatarSrc: avatar008,
+    name: "孙文佳",
+    status: "offline",
+    time: "下午 14:45",
+    message: "好的，明天我就把资料送过去！感谢提醒！",
+    unreadCount: 2,
+    replied: false,
+  },
+  {
+    id: 9,
+    avatarSrc: avatar009,
+    name: "慕容天宇",
+    status: "offline",
+    time: "早上 06:18",
+    message: "明天约一把王者荣耀，不连赢5把不罢休 🤘",
+    unreadCount: 0,
+    replied: true,
+  },
+  {
+    id: 10,
+    avatarSrc: avatar010,
+    name: "郭文菲",
+    status: "online",
+    time: "2018年9月5日",
+    message: "明天我要去上课，如果有空的话我再和你说",
+    unreadCount: 0,
+    replied: true,
+  },
+  {
+    id: 11,
+    avatarSrc: avatar011,
+    name: "张峰",
+    status: "online",
+    time: "2018年9月1日",
+    message: "公司打印机坏了，找个人维修一下",
+    unreadCount: 0,
+    replied: false,
+  },
+  {
+    id: 12,
+    avatarSrc: avatar012,
+    name: "李凝",
+    status: "offline",
+    time: "2018年8月31日",
+    message: "你知道吗？昨天发生了一件很神奇的事情！",
+    unreadCount: 0,
+    replied: true,
+  },
+  {
+    id: 13,
+    avatarSrc: avatar013,
+    name: "李铭浩",
+    status: "online",
+    time: "3 小时之前",
+    message: "即使爬到最高的山上，一次也只能脚踏实地地迈一步",
+    unreadCount: 2,
+    replied: false,
+  },
+  {
+    id: 14,
+    avatarSrc: avatar014,
+    name: "孙文佳",
+    status: "offline",
+    time: "下午 14:45",
+    message: "好的，明天我就把资料送过去！感谢提醒！",
+    unreadCount: 2,
+    replied: false,
+  },
+  {
+    id: 15,
+    avatarSrc: avatar015,
+    name: "慕容天宇",
+    status: "offline",
+    time: "早上 06:18",
+    message: "明天约一把王者荣耀，不连赢5把不罢休 🤘",
+    unreadCount: 0,
+    replied: true,
+  },
+  {
+    id: 16,
+    avatarSrc: avatar016,
+    name: "郭文菲",
+    status: "online",
+    time: "2018年9月5日",
+    message: "明天我要去上课，如果有空的话我再和你说",
+    unreadCount: 0,
+    replied: true,
+  },
+  {
+    id: 17,
+    avatarSrc: avatar017,
+    name: "张峰",
+    status: "online",
+    time: "2018年9月1日",
+    message: "公司打印机坏了，找个人维修一下",
+    unreadCount: 0,
+    replied: false,
+  },
+  {
+    id: 18,
+    avatarSrc: avatar018,
+    name: "李凝",
+    status: "offline",
+    time: "2018年8月31日",
+    message: "你知道吗？昨天发生了一件很神奇的事情！",
+    unreadCount: 0,
+    replied: true,
+  },
+  {
+    id: 19,
+    avatarSrc: avatar019,
+    name: "张峰",
+    status: "online",
+    time: "2018年9月1日",
+    message: "公司打印机坏了，找个人维修一下",
+    unreadCount: 0,
+    replied: false,
+  },
+  {
+    id: 20,
+    avatarSrc: avatar020,
+    name: "李凝",
+    status: "offline",
+    time: "2018年8月31日",
+    message: "你知道吗？昨天发生了一件很神奇的事情！",
+    unreadCount: 0,
+    replied: true,
+  },
+];
