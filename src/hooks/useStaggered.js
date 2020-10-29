07c868afd1fddb5c8044bf7313da251d9525d736
Actiew <wuwuzgr@gmail.com>
2020-10-29 21:47:06 +0800

    React Spring动画
    Signed-off-by: Actiew <wuwuzgr@gmail.com>

---
+++ src/hooks/useStaggered.js
@@ -0,0 +1,15 @@
+import { useTrail } from "react-spring";
+
+export default function useStaggeredList(count) {
+   const trailAnimes = useTrail(count, {
+      transform: "translate3d(0, 0, 0)",
+      from: {transform: "translate3d(-424px, 0, 0)"},
+      config: {
+         mass: 0.9,
+         tension: 280,
+         friction: 24,
+      },
+      delay: 0,
+   });
+   return trailAnimes;
+}
